deb9af701ab7c7fc2ae1d2ef05a19db8ce3374f3
MuntasirSunny <hossainmdmuntasir@gmail.com>
2025-05-08 16:21:09 +0600

    ui-fixes

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const postMetadata = getPostMetadata("blogs");
 
   return (
-    <div className="container mx-auto h-screen flex flex-1 justify-center items-center">
+    <div className="flex flex-1 justify-center items-center mt-[20px]">
       <div className="flex flex-col items-center gap-2 lg:gap-5 mx-2 lg:mx-0">
         {postMetadata.map((post, index) => {
           return <BlogCard key={index} post={post} />;
